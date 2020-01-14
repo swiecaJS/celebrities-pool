90b8399601d992d4a2646bede13d3aafd5946357
swiecaJS <karol.swieca@netguru.co>
2020-01-14 22:14:02 +0100

    feat(Layout) - global component

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import styles from "./Layout.module.scss";
+
+const propTypes = {};
+const defaultProps = {};
+
+type Props = PropTypes.InferProps<typeof propTypes>;
+
+const Layout: React.FC<Props> = ({ children }) => {
+  return (
+    <div className={styles.wrapper}>
+      <main className={styles.game}>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+Layout.propTypes = propTypes;
+Layout.defaultProps = defaultProps;
+
+export default Layout;
--- src/root/root.tsx
+++ src/root/root.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import Layout from 'components/Layout/Layout'
+
 import Start from 'pages/Start/Start'
 import GameRules from 'pages/GameRules/GameRules'
 
-import styles from "./root.module.scss";
+import "./root.module.scss";
 
 const propTypes = {};
 const defaultProps = {};
@@ -14,18 +16,18 @@ type Props = PropTypes.InferProps<typeof propTypes>;
 
 const root: React.FC<Props> = () => {
   return (
-    <main className={styles.page}>
       <Router>
         <Switch>
-          <Route path="/" exact>
-            <Start />
-          </Route>
-          <Route path="/rules">
-            <GameRules />
-          </Route>
+          <Layout>
+            <Route path="/" exact>
+              <Start />
+            </Route>
+            <Route path="/rules">
+              <GameRules />
+            </Route>
+          </Layout>
         </Switch>
       </Router>
-    </main>
   );
 };
 
