65dc86ad5b6d48b0d338d4bd5c3b2f830820c9b7
swiecaJS <karol.swieca@netguru.co>
2020-01-17 18:30:38 +0100

    feat(Start Page) - updated layout

--- src/pages/Start/Start.tsx
+++ src/pages/Start/Start.tsx
@@ -1,13 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import BaseText from "components/BaseText/BaseText";
+import BaseLink from "components/BaseLink/BaseLink";
+
+import styles from "./Start.module.scss";
+
 const propTypes = {};
 const defaultProps = {};
 
 type Props = PropTypes.InferProps<typeof propTypes>;
 
 const Start: React.FC<Props> = props => {
-  return <div>Start</div>;
+  return (
+    <section className={styles.wrapper}>
+      <BaseText tag="h1" size={7} isBold>
+        Sack of Celebrities
+        <span role="img" aria-label="money sack">
+          💰
+        </span>
+      </BaseText>
+      <BaseLink to="/rules" type="primary">
+        Start
+      </BaseLink>
+    </section>
+  );
 };
 
 Start.propTypes = propTypes;
