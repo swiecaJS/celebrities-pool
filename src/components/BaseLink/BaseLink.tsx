7cd03bf5557545e3b02a1c6d419dfb415117f053
swiecaJS <karol.swieca@netguru.co>
2020-01-18 21:05:27 +0100

    feat(BaseLink) - accept className prop

--- src/components/BaseLink/BaseLink.tsx
+++ src/components/BaseLink/BaseLink.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import cn from 'classnames'
 
 import BaseText from "components/BaseText/BaseText";
 
@@ -8,11 +9,12 @@ import styles from "./BaseLink.module.scss";
 interface Props {
   type: "primary";
   to: string
+  className?: string
 }
 
-const BaseLink: React.FC<Props> = ({ children, type, to }) => {
+const BaseLink: React.FC<Props> = ({ children, type, to, className }) => {
   return (
-    <Link to={to} className={styles[type]}>
+    <Link to={to} className={cn(styles[type], className)}>
       <BaseText tag="span" size={5} className={styles[`${type}Text`]} isBold>
         {children}
       </BaseText>
