764ac40733269593367d9cfaed6062d90f895816
swiecaJS <karol.swieca@netguru.co>
2020-01-18 18:25:34 +0100

    feat(BaseSlider) - react-slider used to create controlled sliders

---
+++ src/components/BaseSlider/BaseSlider.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import ReactSlider from "react-slider";
+
+import BaseText from "components/BaseText/BaseText";
+
+import styles from "./BaseSlider.module.scss";
+
+
+interface Props  {
+  handleChange: (value: number) => void;
+  value: number | undefined;
+  defaultValue?: number | undefined;
+  max?: number | undefined;
+  min?: number | undefined;
+  step?: number | undefined;
+  label: string;
+  wrapperClassName?: string;
+}
+
+const BaseSlider: React.FC<Props> = ({
+  handleChange,
+  label,
+  wrapperClassName,
+  value,
+  min,
+  max,
+  step,
+  defaultValue
+}) => {
+  return (
+    <div className={wrapperClassName || ""}>
+      <BaseText tag="p" size={4}>
+        {label}
+      </BaseText>
+      <ReactSlider
+        min={min}
+        max={max}
+        step={step}
+        value={value}
+        className={styles.slider}
+        trackClassName={styles.track}
+        thumbClassName={styles.thumb}
+        renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+        onAfterChange={value => {
+          const numberValue = value as number;
+          handleChange(numberValue);
+        }}
+      />
+    </div>
+  );
+};
+
+export default BaseSlider;
