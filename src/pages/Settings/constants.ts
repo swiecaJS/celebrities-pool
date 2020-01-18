9a30ba05001cda7492f1763cbc393a8a17050924
swiecaJS <karol.swieca@netguru.co>
2020-01-18 21:02:07 +0100

    feat(Settings) - parameters constants

---
+++ src/pages/Settings/constants.ts
@@ -0,0 +1,23 @@
+export const CHARACTERS_PER_PERSON = {
+  MIN: 1,
+  MAX: 10,
+  STEP: 1
+} as const;
+
+export const NUMBER_OF_PLAYERS = {
+  MIN: 4,
+  MAX: 20,
+  STEP: 1
+} as const;
+
+export const SECONDS_FOR_ROUND = {
+  MIN: 10,
+  MAX: 120,
+  STEP: 5
+} as const;
+
+export const SECONDS_FOR_CHARADES = {
+  MIN: 10,
+  MAX: 120,
+  STEP: 5
+} as const;
