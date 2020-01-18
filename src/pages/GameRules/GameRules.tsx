94ee01b7f5742d90239077e779776a63ea650284
swiecaJS <karol.swieca@netguru.co>
2020-01-18 21:06:27 +0100

    feat(GameRules) - basic layout

--- src/pages/GameRules/GameRules.tsx
+++ src/pages/GameRules/GameRules.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import routes from "constants/routes";
+
+import BaseText from "components/BaseText/BaseText";
+import BaseLink from "components/BaseLink/BaseLink";
+
 const propTypes = {};
 const defaultProps = {};
 
 type Props = PropTypes.InferProps<typeof propTypes>;
 
 const GameRules: React.FC<Props> = props => {
-  return <div>GameRules</div>;
+  return (
+    <section>
+      <BaseText tag="h1" size={6}>
+        Do you know how to play?
+      </BaseText>
+      <BaseLink to={routes.settings} type="primary">
+        Yes
+      </BaseLink>
+    </section>
+  );
 };
 
 GameRules.propTypes = propTypes;
