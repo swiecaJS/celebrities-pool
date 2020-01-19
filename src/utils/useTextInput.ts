1f1625980e06ced33f1a0e9c83261aa775e5e52b
swiecaJS <karol.swieca@netguru.co>
2020-01-19 19:41:23 +0100

    feat(useTextInput) - utility for binding text input values

---
+++ src/utils/useTextInput.ts
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export default function useInput(initialValue: string) {
+  const [value, setValue] = useState(initialValue);
+
+  return {
+    value,
+    setValue,
+    reset: () => setValue(""),
+    bind: {
+      value,
+      onChange: (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value);
+      }
+    }
+  };
+};
