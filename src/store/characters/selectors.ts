d429cf49a368321fc926e8ef80268235484ac3cc
swiecaJS <karol.swieca@netguru.co>
2020-01-19 19:42:09 +0100

    feat(characters store) - added selectors | mounted in root reducer

---
+++ src/store/characters/selectors.ts
@@ -0,0 +1,13 @@
+import { useSelector } from "react-redux";
+
+import { AppState } from "../types";
+import { CharactersState, Character } from "./types";
+import { initialState } from "./reducer";
+
+const getStateSubtree = (state: AppState): CharactersState =>
+  state.characters || initialState;
+
+export const getCharacters = (state: AppState): Character[] =>
+  getStateSubtree(state).characters;
+
+export const useGetCharacters = () => useSelector(getCharacters);
--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import settingsReducer from "store/settings/reducer";
+import charactersReducer from 'store/characters/reducer'
 
 const rootReducer = combineReducers({
-  settings: settingsReducer
+  settings: settingsReducer,
+  characters: charactersReducer,
 });
 
 export default rootReducer;
