f98fccba6de69497393974c703d50b6582011ccf
swiecaJS <karol.swieca@netguru.co>
2020-01-23 20:54:10 +0100

    feat(game store) - remove actionTypes -> move to strings

--- src/store/game/actions.ts
+++ src/store/game/actions.ts
@@ -4,28 +4,30 @@ import { TeamKey, Round } from "./types";
 
 export const initialSetup = createAction('game/INITIAL_SETUP')
 
-export const addPoints = createAction<number, typeof actionTypes.ADD_POINTS>(
-  actionTypes.ADD_POINTS
+export const addPoints = createAction<number, 'game/ADD_POINTS'>(
+  'game/ADD_POINTS'
 );
 
-export const setIsReady = createAction<boolean, typeof actionTypes.SET_IS_READY>(
-  actionTypes.SET_IS_READY
+export const setIsReady = createAction<boolean, 'game/SET_IS_READY'>(
+  'game/SET_IS_READY'
 );
 
-export const setRound = createAction<Round, typeof actionTypes.SET_ROUND>(
-  actionTypes.SET_ROUND
+export const setRound = createAction<Round,'game/SET_ROUND'>(
+  'game/SET_ROUND'
 );
+
+export const setCurrentCharacter = createAction<string, 'game/SET_CURRENT_CHARACTER'>('SET_CURRENT_CHARACTER')
 
 export const setTeamGuessing = createAction<
   TeamKey,
-  typeof actionTypes.SET_TEAM_GUESSING
->(actionTypes.SET_TEAM_GUESSING);
+  'game/SET_TEAM_GUESSING'
+>('game/SET_TEAM_GUESSING');
 
-export const setTimeLeft = createAction<number, typeof actionTypes.SET_TIME_LEFT>(
-  actionTypes.SET_TIME_LEFT
+export const setTimeLeft = createAction<number,'game/SET_TIME_LEFT'>(
+'game/SET_TIME_LEFT'
 );
 
 export const setCharactersLeftToGuess = createAction<
   string[],
-  typeof actionTypes.SET_CHARACTERS_LEFT_TO_GUESS
->(actionTypes.SET_CHARACTERS_LEFT_TO_GUESS);
+  'game/SET_CHARACTERS_LEFT_TO_GUESS'
+>('game/SET_CHARACTERS_LEFT_TO_GUESS');
