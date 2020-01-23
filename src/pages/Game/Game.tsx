1a686833135662bae99fbf7cdf7ebf181d5bc82c
swiecaJS <karol.swieca@netguru.co>
2020-01-23 22:55:31 +0100

    feat(GameLayout) - extract Header and hook it with store

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,48 +1,35 @@
 import React from "react";
-import {useDispatch} from 'react-redux'
-import { useTranslation } from "react-i18next";
+import { useDispatch } from "react-redux";
 
-import {initialSetup} from 'store/game/actions'
+import { initialSetup } from "store/game/actions";
 
-import BaseText from "components/BaseText/BaseText";
-import ScoreBoard from './ScoreBoard'
-import Controls from './Controls'
+import ScoreBoard from "./ScoreBoard";
+import Controls from "./Controls";
+import CharacterToGuess from "./CharacterToGuess";
+import Header from "./Header";
 
 import styles from "./Game.module.scss";
 
 const Start: React.FC = () => {
-  const { t } = useTranslation("game");
   const dispatch = useDispatch();
-  const onInitalSetup = React.useCallback(() => {
+  const onInitialSetup = React.useCallback(() => {
     dispatch(initialSetup());
-  }, [dispatch])
+  }, [dispatch]);
 
   React.useEffect(() => {
-    onInitalSetup()
-  }, [onInitalSetup])
+    onInitialSetup();
+  }, [onInitialSetup]);
 
   return (
     <section className={styles.wrapper}>
       <div>
-        <BaseText tag="h1" size={6} isBold className={styles.header}>
-          {t("ROUND 1 - TEAM A")}
-        </BaseText>
+        <Header />
         <ScoreBoard />
         <div className={styles.progressBar}>
           <span className={styles.progressBarMeter}></span>
         </div>
       </div>
-      <div className={styles.character}>
-        <BaseText tag="p" size={3} className={styles.characterHelp}>
-          In this round, use as many words as you wish to describe a character
-          😎
-        </BaseText>
-        <div>
-          <BaseText tag="h3" size={8} className={styles.characterText}>
-            Batman
-          </BaseText>
-        </div>
-      </div>
+      <CharacterToGuess />
       <Controls />
     </section>
   );
---
+++ src/pages/Game/Header.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+import { useGetCurrentTeam, useGetRound } from "store/game/selectors";
+
+import BaseText from "components/BaseText/BaseText";
+
+import styles from "./Game.module.scss";
+
+const Header: React.FC = () => {
+  const { t } = useTranslation("game");
+  const team = useGetCurrentTeam();
+  const round = useGetRound();
+
+  return (
+    <BaseText tag="h1" size={6} isBold className={styles.header}>
+      {t(`ROUND ${round} - TEAM ${team}`)}
+    </BaseText>
+  );
+};
+
+export default Header;
