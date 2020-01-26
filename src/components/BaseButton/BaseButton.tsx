c71e4b6bc6586dd56674007dac31aafa239f0767
swiecaJS <karol.swieca@netguru.co>
2020-01-26 17:47:19 +0100

    feat(Game components) - add some data-cy selectors

--- src/components/BaseButton/BaseButton.tsx
+++ src/components/BaseButton/BaseButton.tsx
@@ -2,17 +2,28 @@ import React from "react";
 
 import BaseText from "components/BaseText/BaseText";
 
-import styles from './BaseButton.module.scss'
+import styles from "./BaseButton.module.scss";
 
 interface Props {
   onClick?: () => void;
   type: "submit" | "button";
   className?: "string";
+  cypressSelector?: string;
 }
 
-const BaseButton: React.FC<Props> = ({ onClick, type, children }) => {
+const BaseButton: React.FC<Props> = ({
+  onClick,
+  type,
+  children,
+  cypressSelector
+}) => {
   return (
-    <button onClick={onClick} type={type} className={styles.primary}>
+    <button
+      onClick={onClick}
+      type={type}
+      className={styles.primary}
+      data-cy={cypressSelector}
+    >
       <BaseText tag="span" size={5} className={styles.primaryText} isBold>
         {children}
       </BaseText>
