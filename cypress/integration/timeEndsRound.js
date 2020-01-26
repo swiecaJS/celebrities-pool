87ab1f19a6e1af75c1ef24a732c0cb33644de6cc
swiecaJS <karol.swieca@netguru.co>
2020-01-26 20:00:58 +0100

    chore(e2e) - lint

--- cypress/integration/timeEndsRound.js
+++ cypress/integration/timeEndsRound.js
@@ -1,92 +1,93 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /// <reference types="Cypress" />
-import faker from "faker";
+import faker from 'faker';
 
 const SLIDER_STEP = 1;
 const MIN_NUMBER_OF_CHARACTERS_PER_PLAYER = 1;
 const MIN_TIME_PER_ROUND_IN_SECONDS = 10;
 
-describe("Time for turn ends", () => {
+describe('Time for turn ends', () => {
   let charactersPerPerson;
   let numberOfPlayers;
 
   beforeEach(() => {
-    cy.viewport("iphone-6");
+    cy.viewport('iphone-6');
   });
 
-  it("shows start screem", function() {
+  it('shows start screen', () => {
     cy.visit(Cypress.config().baseUrl);
     cy.get('[data-cy="start-game-btn"]').click();
-    cy.url().should("eq", `${Cypress.config().baseUrl}/rules`);
+    cy.url().should('eq', `${Cypress.config().baseUrl}/rules`);
   });
 
-  it("ask users about rules", () => {
+  it('ask users about rules', () => {
     cy.get('[data-cy="user-know-rules-btn"]').click();
-    cy.url().should("eq", `${Cypress.config().baseUrl}/settings`);
+    cy.url().should('eq', `${Cypress.config().baseUrl}/settings`);
   });
 
-  it("allows to change settings", () => {
+  it('allows to change settings', () => {
     let sliderValue;
     // TO DO - create command
     cy.get('[data-cy="number-of-players-slider"]')
-      .then($div => {
+      .then(($div) => {
         sliderValue = Number($div.text());
       })
-      .trigger("mousedown")
-      .trigger("mousemove", "right")
-      .trigger("mouseup")
-      .should($div => {
+      .trigger('mousedown')
+      .trigger('mousemove', 'right')
+      .trigger('mouseup')
+      .should(($div) => {
         numberOfPlayers = Number($div.text());
         expect(numberOfPlayers).to.eq(sliderValue + SLIDER_STEP);
       });
 
     cy.get('[data-cy="character-per-person-slider"]')
-      .then($div => {
+      .then(($div) => {
         sliderValue = Number($div.text());
       })
-      .trigger("mousedown")
-      .trigger("mousemove", "left")
-      .trigger("mousemove", "left")
-      .trigger("mousemove", "left")
-      .trigger("mousemove", "left")
-      .trigger("mousemove", "left")
-      .trigger("mouseup")
-      .should($div => {
+      .trigger('mousedown')
+      .trigger('mousemove', 'left')
+      .trigger('mousemove', 'left')
+      .trigger('mousemove', 'left')
+      .trigger('mousemove', 'left')
+      .trigger('mousemove', 'left')
+      .trigger('mouseup')
+      .should(($div) => {
         charactersPerPerson = Number($div.text());
         expect(charactersPerPerson).to.eq(MIN_NUMBER_OF_CHARACTERS_PER_PLAYER);
       });
 
     cy.get('[data-cy="seconds-for-round-slider"]')
       .then(($div) => {
-        sliderValue = Number($div.text())
-        console.log('sliderValue', sliderValue)
+        sliderValue = Number($div.text());
+        console.log('sliderValue', sliderValue);
       })
-      .trigger("mousedown")
-      .trigger("mousemove", 'left')
-      .trigger("mousemove", 'left')
-      .trigger("mousemove", 'left')
-      .trigger("mousemove", 'left')
-      .trigger("mouseup")
+      .trigger('mousedown')
+      .trigger('mousemove', 'left')
+      .trigger('mousemove', 'left')
+      .trigger('mousemove', 'left')
+      .trigger('mousemove', 'left')
+      .trigger('mouseup')
       .should(($div) => {
-        const currentValue = Number($div.text())
-        expect(currentValue).to.eq(MIN_TIME_PER_ROUND_IN_SECONDS)
-      })
+        const currentValue = Number($div.text());
+        expect(currentValue).to.eq(MIN_TIME_PER_ROUND_IN_SECONDS);
+      });
 
     cy.get('[data-cy="settings-finished-btn"').click();
   });
 
-  it("asks users to enter characters", () => {
+  it('asks users to enter characters', () => {
     const totalCharactersNumber = charactersPerPerson * numberOfPlayers;
-    cy.url().should("eq", `${Cypress.config().baseUrl}/characters`);
+    cy.url().should('eq', `${Cypress.config().baseUrl}/characters`);
 
     cy.get('[data-cy="total-characters-in-game"]')
-      .invoke("val")
+      .invoke('val')
       .then(value => expect(Number(value)).to.eq(totalCharactersNumber));
 
     let enteredCharacters = 0;
 
     cy.wrap(new Array(totalCharactersNumber)).each(() => {
       cy.get('[data-cy="characters-entered"]')
-        .invoke("val")
+        .invoke('val')
         .then(value => expect(Number(value)).to.eq(enteredCharacters));
 
       cy.get('[data-cy="player-ready-btn"]').click();
@@ -101,26 +102,25 @@ describe("Time for turn ends", () => {
     cy.get('[data-cy="characters-entering-finished"]').click();
   });
 
-  it("display info about first round", () => {
-    cy.url().should("eq", `${Cypress.config().baseUrl}/game`);
-    cy.get('[data-cy="round-opening"]').should("be.visible");
-    cy.get('[data-cy="game-standings"]').should("not.be.visible");
+  it('display info about first round', () => {
+    cy.url().should('eq', `${Cypress.config().baseUrl}/game`);
+    cy.get('[data-cy="round-opening"]').should('be.visible');
+    cy.get('[data-cy="game-standings"]').should('not.be.visible');
     cy.get('[data-cy="start-round-btn"]').click();
   });
 
-  it("when time for turn ends, next turn button is visible", () => {
+  it('when time for turn ends, next turn button is visible', () => {
     const correctBtn = 'button[data-cy="correct"]';
     const notCorrectBtn = 'button[data-cy="notCorrect"]';
 
-    cy.url().should("eq", `${Cypress.config().baseUrl}/game`);
-    cy.get('[data-cy="round-opening"]').should("not.be.visible");
-    cy.get('[data-cy="game-standings"]').should("be.visible");
+    cy.url().should('eq', `${Cypress.config().baseUrl}/game`);
+    cy.get('[data-cy="round-opening"]').should('not.be.visible');
+    cy.get('[data-cy="game-standings"]').should('be.visible');
     cy.get('[data-cy="start-turn-btn"]').click();
 
     cy.get(notCorrectBtn).should('be.visible');
     cy.get(correctBtn).should('be.visible');
     cy.wait(MIN_TIME_PER_ROUND_IN_SECONDS * 1000);
     cy.get('[data-cy="start-turn-btn"]').should('be.visible');
-
   });
 });
