2d5ce9dea082e9e865f5220fa62987acff5eecac
swiecaJS <karol.swieca@netguru.co>
2020-02-05 17:47:25 +0100

    feat(BaseButton) - can be disabled

--- src/components/BaseButton/BaseButton.tsx
+++ src/components/BaseButton/BaseButton.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cn from 'classnames';
 
 import BaseText from 'components/BaseText/BaseText';
 
@@ -9,6 +10,7 @@ interface Props {
   type: 'submit' | 'button';
   className?: 'string';
   cypressSelector?: string;
+  isDisabled?: boolean;
 }
 
 const BaseButton: React.FC<Props> = ({
@@ -16,12 +18,14 @@ const BaseButton: React.FC<Props> = ({
   type,
   children,
   cypressSelector,
+  isDisabled,
 }) => (
   <button
     onClick={onClick}
     type={type}
-    className={styles.primary}
+    className={cn(styles.primary, { [styles.disabled]: isDisabled })}
     data-cy={cypressSelector}
+    disabled={isDisabled}
   >
     <BaseText tag="span" size={5} className={styles.primaryText} isBold>
       {children}
