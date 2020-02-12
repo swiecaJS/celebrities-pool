d875ca0a883b4030cd2509c3cc58b7a2e6229024
swiecaJS <karol.swieca@netguru.co>
2020-02-12 20:31:43 +0100

    feat(root) - extract app routes

--- src/root/root.tsx
+++ src/root/root.tsx
@@ -1,61 +1,23 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
-import routes from 'constants/routes';
 import { store, persistor } from 'store';
 
-import Layout from 'components/Layout/Layout';
-
-import Start from 'pages/Start/Start';
-import GameRules from 'pages/GameRules/GameRules';
-import Settings from 'pages/Settings/Settings';
-import Characters from 'pages/Characters/Characters';
-import Game from 'pages/Game/Game';
-import Results from 'pages/Results/Results';
+import AppRoutes from './AppRoutes';
 
 import 'config/translations/i18n';
 import './root.module.scss';
 
-const propTypes = {};
-const defaultProps = {};
-
-type Props = PropTypes.InferProps<typeof propTypes>;
-
-const root: React.FC<Props> = () => (
+const Root: React.FC = () => (
   <Provider store={store}>
     <PersistGate persistor={persistor}>
       <Router>
-        <Switch>
-          <Layout>
-            <Route path={routes.start} exact>
-              <Start />
-            </Route>
-            <Route path={routes.rules}>
-              <GameRules />
-            </Route>
-            <Route path={routes.settings}>
-              <Settings />
-            </Route>
-            <Route path={routes.characters}>
-              <Characters />
-            </Route>
-            <Route path={routes.game}>
-              <Game />
-            </Route>
-            <Route path={routes.results}>
-              <Results />
-            </Route>
-          </Layout>
-        </Switch>
+        <AppRoutes />
       </Router>
     </PersistGate>
   </Provider>
 );
 
-root.propTypes = propTypes;
-root.defaultProps = defaultProps;
-
-export default root;
+export default Root;
