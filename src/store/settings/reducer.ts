80d9d80f3116b45c9147e7d963511dc6c550f562
swiecaJS <karol.swieca@netguru.co>
2020-03-11 12:33:50 +0100

    feat(Settings Store) - rewrite reducer with reduxjs toolkit

--- src/store/settings/reducer.ts
+++ src/store/settings/reducer.ts
@@ -1,46 +1,62 @@
-import { AnyAction } from 'redux';
+import { createReducer } from '@reduxjs/toolkit';
 
-import { SettingsState, SettingsActionTypes } from './types';
-import { SET_CHARACTERS_PER_PERSON, SET_SECONDS_FOR_CHARADES, SET_SECONDS_FOR_ROUND, SET_NUMBER_OF_PLAYERS } from './actionTypes';
+import * as settingActions from './actions';
+
+export interface SettingsState {
+  numberOfPlayers: number;
+  charactersPerPerson: number;
+  secondsForRound: number;
+}
 
 export const initialState: SettingsState = {
   numberOfPlayers: 4,
   charactersPerPerson: 4,
   secondsForRound: 30,
-  secondsForCharades: 45,
 };
 
-export default (
-  state = initialState,
-  incomingAction: AnyAction,
-): SettingsState => {
-  const action = incomingAction as SettingsActionTypes;
-  switch (action.type) {
-    case SET_CHARACTERS_PER_PERSON: {
-      return {
-        ...state,
-        charactersPerPerson: action.payload.charactersPerPerson,
-      };
-    }
-    case SET_SECONDS_FOR_CHARADES: {
-      return {
-        ...state,
-        secondsForCharades: action.payload.secondsForCharades,
-      };
-    }
-    case SET_SECONDS_FOR_ROUND: {
-      return {
-        ...state,
-        secondsForRound: action.payload.secondsForRound,
-      };
-    }
-    case SET_NUMBER_OF_PLAYERS: {
-      return {
-        ...state,
-        numberOfPlayers: action.payload.numberOfPlayers,
-      };
-    }
-    default:
-      return state;
-  }
-};
+// export default (
+//   state = initialState,
+//   incomingAction: AnyAction,
+// ): SettingsState => {
+//   const action = incomingAction as SettingsActionTypes;
+//   switch (action.type) {
+//     case SET_CHARACTERS_PER_PERSON: {
+//       return {
+//         ...state,
+//         charactersPerPerson: action.payload.charactersPerPerson,
+//       };
+//     }
+//     case SET_SECONDS_FOR_ROUND: {
+//       return {
+//         ...state,
+//         secondsForRound: action.payload.secondsForRound,
+//       };
+//     }
+//     case SET_NUMBER_OF_PLAYERS: {
+//       return {
+//         ...state,
+//         numberOfPlayers: action.payload.numberOfPlayers,
+//       };
+//     }
+//     default:
+//       return state;
+//   }
+// };
+
+const reducer = createReducer(initialState, builder => builder.addCase(
+  settingActions.setCharactersPerPerson,
+  (state, { payload }) => ({
+    ...state,
+    charactersPerPerson: payload,
+  }),
+)
+  .addCase(settingActions.setNumberOfPlayers, (state, { payload }) => ({
+    ...state,
+    numberOfPlayers: payload,
+  }))
+  .addCase(settingActions.setSecondsForRound, (state, { payload }) => ({
+    ...state,
+    secondsForRound: payload,
+  })));
+
+export default reducer;
