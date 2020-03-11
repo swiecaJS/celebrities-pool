80944b09f2a8d4967d61257244f6914ea2de5c56
swiecaJS <karol.swieca@netguru.co>
2020-03-11 12:33:22 +0100

    feat(Settings Store) - rewrite actions with reduxjs toolkit

--- src/store/settings/actionTypes.ts
+++
@@ -1,4 +0,0 @@
-export const SET_NUMBER_OF_PLAYERS = 'game/SET_NUMBER_OF_PLAYERS' as const;
-export const SET_CHARACTERS_PER_PERSON = 'game/SET_CHARACTERS_PER_PERSON' as const;
-export const SET_SECONDS_FOR_ROUND = 'game/SET_SECONDS_FOR_ROUND' as const;
-export const SET_SECONDS_FOR_CHARADES = 'game/SET_SECONDS_FOR_CHARADES' as const;
--- src/store/settings/actions.ts
+++ src/store/settings/actions.ts
@@ -1,41 +1,5 @@
-import * as actionTypes from './actionTypes';
-import {
-  SetCharactersPerPersonAction,
-  SetNumberOfPlayersAction,
-  SetSecondsForRoundAction,
-  SetSecondsForCharadesAction,
-} from './types';
+import { createAction } from '@reduxjs/toolkit';
 
-export const setCharactersPerPerson = (
-  charactersPerPerson: number,
-): SetCharactersPerPersonAction => ({
-  type: actionTypes.SET_CHARACTERS_PER_PERSON,
-  payload: {
-    charactersPerPerson,
-  },
-});
-
-export const setNumberOfPlayers = (
-  numberOfPlayers: number,
-): SetNumberOfPlayersAction => ({
-  type: actionTypes.SET_NUMBER_OF_PLAYERS,
-  payload: {
-    numberOfPlayers,
-  },
-});
-export const setSecondsForRound = (
-  secondsForRound: number,
-): SetSecondsForRoundAction => ({
-  type: actionTypes.SET_SECONDS_FOR_ROUND,
-  payload: {
-    secondsForRound,
-  },
-});
-export const setSecondsForCharades = (
-  secondsForCharades: number,
-): SetSecondsForCharadesAction => ({
-  type: actionTypes.SET_SECONDS_FOR_CHARADES,
-  payload: {
-    secondsForCharades,
-  },
-});
+export const setCharactersPerPerson = createAction<number, 'game/SET_CHARACTERS_PER_PERSON'>('game/SET_CHARACTERS_PER_PERSON');
+export const setNumberOfPlayers = createAction<number, 'game/SET_NUMBER_OF_PLAYERS'>('game/SET_NUMBER_OF_PLAYERS');
+export const setSecondsForRound = createAction<number, 'game/SET_SECONDS_FOR_ROUND'>('game/SET_SECONDS_FOR_ROUND');
