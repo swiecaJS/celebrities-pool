4757c38fa96518c9661e35b4e722e3e1a38e3cfc
swiecaJS <karol.swieca@netguru.co>
2020-03-20 19:15:10 +0100

    layout(AddingCharacters) - added some padding into text input

--- src/pages/Characters/Finished.tsx
+++ src/pages/Characters/Finished.tsx
@@ -3,18 +3,14 @@ import { useTranslation } from 'react-i18next';
 
 import routes from 'constants/routes';
 
-import BaseText from 'components/BaseText/BaseText';
 import BaseLink from 'components/BaseLink/BaseLink';
 
 const Finished: React.FC = () => {
-  const { t } = useTranslation('characters');
+  const { t } = useTranslation('addingCharacters');
   return (
-    <>
-      <BaseText tag="h3">{t('finished')}</BaseText>
-      <BaseLink to={routes.game} type="primary" cypressSelector="characters-entering-finished">
-        {t('start')}
-      </BaseLink>
-    </>
+    <BaseLink to={routes.game} type="primary" cypressSelector="characters-entering-finished">
+      {t('start')}
+    </BaseLink>
   );
 };
 
