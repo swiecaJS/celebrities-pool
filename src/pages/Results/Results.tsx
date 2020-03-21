f0a7ddedf900894b593844ef789bb1d227b7019c
swiecaJS <karol.swieca@netguru.co>
2020-03-21 17:33:32 +0100

    feat(Results) - play again button fade in

--- src/pages/Results/Results.tsx
+++ src/pages/Results/Results.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
@@ -14,18 +14,29 @@ import TeamStandings from './TeamStandings';
 
 import styles from './Results.module.scss';
 
-
 const Results: React.FC = () => {
+  const [canPlayAgain, setCanPlayAgain] = useState(false);
   const { t } = useTranslation('results');
   const history = useHistory();
   const dispatch = useDispatch();
-  const dispatchResetGame = useCallback(() => dispatch(resetGame()),
-    [dispatch]);
+  const dispatchResetGame = useCallback(() => dispatch(resetGame()), [
+    dispatch,
+  ]);
 
   const onResetGame = () => {
     dispatchResetGame();
     history.push(routes.settings);
   };
+
+  useEffect(() => {
+    const TWO_SECONDS = 2000;
+
+    const timeout = setTimeout(() => {
+      setCanPlayAgain(true);
+    }, TWO_SECONDS);
+
+    return () => clearTimeout(timeout);
+  });
 
   return (
     <div data-cy="game-results" className={styles.wrapper}>
@@ -36,10 +47,17 @@ const Results: React.FC = () => {
         <Winner />
         <TeamStandings />
       </div>
-      <BaseButton type="button" onClick={onResetGame} cypressSelector="reset-game-btn">
-        {t('playAgain')}
-      </BaseButton>
-
+      {canPlayAgain ? (
+        <BaseButton
+          className={styles.fade}
+          type="button"
+          onClick={onResetGame}
+          cypressSelector="reset-game-btn"
+        >
+          {t('playAgain')}
+        </BaseButton>
+      // TO DO - fix this dirty hack 😅
+      ) : <div style={{ height: '60px' }} /> }
     </div>
   );
 };
